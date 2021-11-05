b96caa49ca806a0312f751d2f5b84126af5cf477
root <root@AIDENS-PC.localdomain>
2021-11-05 11:28:58 +0000

    feat(syscall.[ch]): bgan syscall_handler implementation

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,10 +1,36 @@
 #include "userprog/syscall.h"
+#include "userprog/process.h"
+#include "userprog/pagedir.c"
+#include "threads/vaddr.h"
 #include <stdio.h>
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "lib/user/syscall.h"
 
 static void syscall_handler (struct intr_frame *);
+
+/* struct syscall_func{
+  int expected_argv;
+  union {
+    void (*fun_ptr)(void);
+    void (*fun_ptr)(int);
+    pid_t exec (const char *file);
+    bool create (const char *file, unsigned initial_size);
+    bool remove (const char *file);
+    int open (const char *file);
+    int filesize (int fd);
+    int read (int fd, void *buffer, unsigned length);
+    void seek (int fd, unsigned position);
+    unsigned tell (int fd);
+    void close (int fd);
+  }
+} */
+
+
+
+uint32_t (*syscall_funcs[])() = {halt, exit, exec, wait, create, remove, open, filesize,
+                             read, write, seek, tell, close};
 
 void
 syscall_init (void) 
@@ -13,8 +39,51 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  int *esp = (int *) f -> esp;
+  int syscall_num = *esp;
+  void *arg1 = *(esp - 12);
+  void *arg2 = *(esp - 8);
+  void *arg3 = *(esp - 4);
+
+
+  uint32_t (*syscall_func)() = syscal l_funcs[syscall_num];
+
+  int argv = (arg1 != NULL) + (arg2 != NULL) + (arg3 != NULL);
+
+  uint32_t result;
+
+  switch (argv)
+  {
+  case 0:
+    result = syscall_func();
+    break;
+  
+  case 1:
+    result = syscall_func(arg1);
+    break;
+
+  case 2:
+    result = syscall_func(arg1,arg2);
+    break;
+
+  case 3:
+    result = syscall_func(arg1,arg2,arg3);
+    break;  
+
+  default:
+    break;
+  }
+
+  f -> eax = result;
+}
+
+void validate_user_pointer (uint32_t *pd, void *user_pointer){
+  if (user_pointer &&  pagedir_get_page(pd,user_pointer) && is_user_vaddr(user_pointer))
+  {
+    return;
+  }
+  //kill the process
+  process_exit();
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,13 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include <inttypes.h>
+#include <stdbool.h>
+
+typedef int pid_t;
+
 void syscall_init (void);
+void validate_user_pointer (uint32_t *pd, void *user_pointer);
+
 
 #endif /* userprog/syscall.h */
