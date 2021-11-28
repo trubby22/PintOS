2f9ff3ff8c319fa53200f8a0b3cdef421be67892
root <root@AIDENS-PC.localdomain>
2021-11-28 11:43:45 +0000

    feat(frame.c): started get_frame function
    BREAKING CHANGE: get_frame missing declarations

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -11,7 +11,8 @@ struct frame_table
     struct hash table;  //
 };
 
-
+//Should a frame table map from frame_ids -> pages
+//or                            page_ids  -> frames?
 struct frame
 {
     //TODO: Missing page property, possibly just a uint32_t
@@ -21,6 +22,10 @@ struct frame
     struct frame *next;      //Pointer to be part of circular queue for eviction
 };
 
+struct frame_table *frame_table;
+frame_table->head  = NULL;
+frame_table->size  = 0;
+hash_init(frame_table->table, hash_hash_func, hash_less_func);
 
 /* 
 Contains:
@@ -45,11 +50,31 @@ Eviction policy
     - If necessary, write the page to the file system or to swap
 */
 
-struct frame *evict (struct frame *head){
-    bool save = head -> save;
-    if (head -> save){
-        
+void* get_frame (uint32_t frameId){
+	//Create dummy frame to seach frame table
+  //Search frame table
+	//if something is returned return it and set save bit to 1
+	//else do below
+	if (frame_table.size == MAX_FRAME_TABLE_SIZE)
+	{
+		struct frame *frame = evict (frame_table.head);
+	} else{
+		//use palloc user page to create a new frame
+		struct frame *head = frame_table -> head;
+		frame -> next  = head -> next;
+		head -> next = frame;
+		frame_table -> head = frame -> next;
+	}
+	
+}
 
-    }
-
+static struct frame *evict (struct frame *head){
+  bool save = head->save;
+  if (save){
+    head -> save = 0;
+    return evict(head->next);
+  }
+	//Allocate swap slot
+	frame_table -> head = head -> next;
+	return head;
 }
