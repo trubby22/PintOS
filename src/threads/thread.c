30468d1661c406612f1c67b9d75f14bd9cdfe6a7
Lewis Kerley <lmk120@sprite36.doc.ic.ac.uk>
2021-12-01 15:29:45 +0000

    feat(thread.c): initialised page_count to 1. ensuring kernel threads still free just 1 page

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -493,6 +493,7 @@ init_thread (struct thread *t, const char *name, int priority)
   t->stack = (uint8_t *) t + PGSIZE;
   t->priority = priority;
   t->magic = THREAD_MAGIC;
+  t->page_count = 1;
   list_init(&t->children);
 
   old_level = intr_disable ();
