03acbaf108ff806a680e1b5ce836b5fbe8f1dda4
AlexB.02 <alexanderdb8@gmail.com>
2021-12-01 16:46:01 +0000

    refactor(process.c): made install_page non static

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -482,8 +482,6 @@ load (const char *file_name, void (**eip) (void), void **esp)
 
 /* load() helpers. */
 
-static bool install_page (void *upage, void *kpage, bool writable);
-
 /* Checks whether PHDR describes a valid, loadable segment in
    FILE and returns true if so, false otherwise. */
 static bool
@@ -737,7 +735,7 @@ static void *init_stack(struct list *args_list) {
    with palloc_get_page().
    Returns true on success, false if UPAGE is already mapped or
    if memory allocation fails. */
-static bool
+bool
 install_page (void *upage, void *kpage, bool writable)
 {
   struct thread *t = thread_current ();
