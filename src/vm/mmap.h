8b48a695305dbf03ed4534ed7b90fa71fcd25a8c
AlexB.02 <alexanderdb8@gmail.com>
2021-12-03 01:56:48 +0000

    feat(mmap.[ch]): implemented head and function definitions for mmap list

--- src/vm/mmap.h
+++ src/vm/mmap.h
@@ -1,4 +1,22 @@
 #ifndef VM_MMAP_H
 #define VM_MMAP_H
 
+#include "lib/user/syscall.h"
+#include <list.h>
+
+// Map a mapid_t to a struct mapped_file
+
+struct mapped_file {
+    mapid_t mapid;                  /* Map id associated with the mapping */
+    int fd;                         /* File descriptor of the file opened */
+    int pgcnt;                      /* Number of continuous pages */
+    void *uaddr;                    /* Address given in syscall */
+    void *kaddr;                    /* Address of kernel pages */
+    struct list_elem map_list_elem; /* Element in the list of mappings */
+};
+
+void mmap_init(void);
+mapid_t mmap_add_mapping(int fd, int pgcnt, void *uaddr, void *kaddr);
+bool mmap_remove_mapping(mapid_t mapid);
+
 #endif
